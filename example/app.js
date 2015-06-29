2a944790fa6a4954f499a6e5993ca9240cc866a5
Lorenzo Villani <lvillani@develer.com>
2015-06-29 11:50:23 +0200

    Reformat example with standard-format

--- example/app.js
+++ example/app.js
@@ -1,15 +1,17 @@
-var app = angular.module('app', ['angular-flot']);
+/* global angular */
 
-app.controller('FlotCtrl', ['$scope', function($scope) {
-  $scope.dataset = [{ data: [], yaxis: 1, label: "sin" }];
+var app = angular.module('app', ['angular-flot'])
+
+app.controller('FlotCtrl', ['$scope', function ($scope) {
+  $scope.dataset = [{ data: [], yaxis: 1, label: 'sin' }]
   $scope.options = {
     legend: {
-      container: "#legend",
+      container: '#legend',
       show: true
     }
-  };
+  }
 
   for (var i = 0; i < 14; i += 0.5) {
-    $scope.dataset[0].data.push([i, Math.sin(i)]);
+    $scope.dataset[0].data.push([i, Math.sin(i)])
   }
-}]);
+}])
