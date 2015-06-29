01bc4b1285babea64fd36681b82b518d356e69e5
Itsik Avidan <itsik.avidan@hp.com>
2015-06-29 14:26:27 +0300

    Adding example to the onClick event

--- example/app.js
+++ example/app.js
@@ -1,15 +1,24 @@
 var app = angular.module('app', ['angular-flot']);
 
-app.controller('FlotCtrl', ['$scope', function($scope) {
-  $scope.dataset = [{ data: [], yaxis: 1, label: "sin" }];
-  $scope.options = {
-    legend: {
-      container: "#legend",
-      show: true
-    }
-  };
+app.controller('FlotCtrl', ['$scope', function ($scope) {
+    $scope.dataset = [{data: [], yaxis: 1, label: "sin"}];
+    $scope.options = {
+        legend: {
+            container: "#legend",
+            show: true
+        },
+        grid: {
+            hoverable: true,
+            clickable: true
+        }
+    };
 
-  for (var i = 0; i < 14; i += 0.5) {
-    $scope.dataset[0].data.push([i, Math.sin(i)]);
-  }
+    for (var i = 0; i < 14; i += 0.5) {
+        $scope.dataset[0].data.push([i, Math.sin(i)]);
+    }
+
+    $scope.onClick = function(event, pos, item){
+        console.log(item.series.data[item.dataIndex]);
+    }
+
 }]);
