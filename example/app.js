39b611175b4957fd9d75f457d772a536b3405cc8
Lorenzo Villani <lvillani@develer.com>
2015-06-29 12:04:52 +0200

    Add pie chart example
    Closes #20

--- example/app.js
+++ example/app.js
@@ -3,6 +3,10 @@
 var app = angular.module('app', ['angular-flot'])
 
 app.controller('FlotCtrl', ['$scope', function ($scope) {
+  //
+  // Standard Chart Example
+  //
+
   $scope.dataset = [{ data: [], yaxis: 1, label: 'sin' }]
   $scope.options = {
     legend: {
@@ -13,5 +17,27 @@ app.controller('FlotCtrl', ['$scope', function ($scope) {
 
   for (var i = 0; i < 14; i += 0.5) {
     $scope.dataset[0].data.push([i, Math.sin(i)])
+  }
+
+  //
+  // Pie Chart Example
+  //
+
+  $scope.pieDataset = []
+  $scope.pieOptions = {
+    series: {
+      pie: {
+        show: true
+      }
+    }
+  }
+
+  var pieSeries = Math.floor(Math.random() * 6) + 3
+
+  for (i = 0; i < pieSeries; i++) {
+    $scope.pieDataset[i] = {
+      label: 'Series' + (i + 1),
+      data: Math.floor(Math.random() * 100) + 1
+    }
   }
 }])
